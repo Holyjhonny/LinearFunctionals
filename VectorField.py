2325142c9128f5b112d53adc2ef8acedccd9ff98
Holyjhonny <71374708+Holyjhonny@users.noreply.github.com>
2021-11-17 19:55:08 +0100

    Added documentation for vector field

---
+++ VectorField.py
@@ -0,0 +1,22 @@
+import numpy as np
+import scipy as sp
+
+# A vector field is a set V that is closed
+# under finite vector addition and
+# scalar multiplication, meaning: Let x,y,z belong to V and
+# let r, s belong to R or C then the following must hold
+
+# x + y = y + x                  Commutativity
+# ( x + y ) + z = x + ( y + z )  Assosiativity
+# 0 + x = x + 0 = = x            Additivity identity
+# x + ( - x ) = 0                Existense of inverse additivity
+# r ( s x ) = ( r s ) x          Assosiativity for scalar
+# ( r + s ) x = r x + s x        Distribution of scalar sums
+#  
+
+
+
+
+
+
+
